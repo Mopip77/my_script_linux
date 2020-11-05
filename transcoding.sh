c294801677c3d389186f20f204e4799b5964392f
Mopip77 <mopip77@qq.com>
2020-11-05 20:17:30 +0800

    Create transcoding.sh

---
+++ transcoding.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+###############################################################
+##               自动同步转码程序                            ##
+##                                                           ##
+##  对于高码率的文件（例如flac和wav格式的音频进行转码）      ##
+##  并在对应的转码文件夹下创建完全一样的文件夹结构           ##
+##                                                           ##
+###############################################################
+
+ROOT_PATH="/Users/bjhl/gogo"          # 音乐文件夹
+ENCODE_PATH="/Users/bjhl/encode"      # 转码后的文件夹，可以不存在
+SUPPORT_EXT=(flac wav)
+
+checkSupportExt() {
+	local type="$1"
+	for supportType in ${SUPPORT_EXT[@]}; do
+		[ "${supportType}" = "${type}" ] && return 0
+	done
+
+	return 1
+}
+
+encode() {
+	local sourceFile="$1"
+	local targetFolder="$2"
+	local fileName="$3"
+
+	echo "transcoding from [${sourceFile}] to [${targetFolder}]"
+	ffmpeg -n -v error -i "${sourceFile}" -ar 44100 -ac 2 -ab 320k "${targetFolder}/${fileName}.aac"
+}
+
+sync() {
+	# 当前文件夹路径
+	local folderPath="$1"
+	local targetPath="$2"
+
+	if [ ! -d "${targetPath}" ]; then
+		echo "mkdir: ${targetPath}"
+		mkdir "${targetPath}"
+	fi
+
+	OLD_IFS="$IFS"
+	IFS=$'\n'
+	for f in `ls "${folderPath}"`; do
+		# 当前文件
+		local file="${folderPath}/${f}"
+
+		[ -L "${file}" ] && continue  # 如果是链接文件直接过滤，怕有死循环
+		[ -d "${file}" ] && sync "${file}" "${targetPath}/${f}"
+		if [ -f "${file}" ]; then
+			local ext="${file##*.}"
+			local fn="${f%.*}"
+			if [ -z `checkSupportExt "${ext}"` ]; then
+				encode "${file}" "${targetPath}" "${fn}"
+			fi
+		fi
+	done
+	IFS=$OLD_IFS
+}
+
+folderPrepare() {
+	if [[ "${ENCODE_PATH}" =~ "${ROOT_PATH}" ]]; then
+		echo "不可以套娃哦！"
+		exit 0
+	fi
+
+	if [ ! -d "${ROOT_PATH}" ]; then
+		echo "您的原文件夹呢？"
+		exit 0
+	fi
+
+	if [ ! -d "${ENCODE_PATH}" ]; then
+		mkdir -p "${ENCODE_PATH}"
+	fi
+}
+
+main() {
+	folderPrepare "${ROOT_PATH}" "${ENCODE_PATH}"
+	sync "${ROOT_PATH}" "${ENCODE_PATH}"
+}
+
+main
